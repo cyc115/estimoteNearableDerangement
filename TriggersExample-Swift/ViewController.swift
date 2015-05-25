7ed64efbb8da54c3ea2924fbe0ae4d6b29013710
macpro <yuechuan20@gmail.com>
2015-05-25 16:57:29 -0400

    working calibration and detection

--- TriggersExample-Swift/ViewController.swift
+++ TriggersExample-Swift/ViewController.swift
@@ -10,13 +10,35 @@ import UIKit
 
 class ViewController: UIViewController, ESTNearableManagerDelegate {
     @IBOutlet var showPopBtn: UIButton!
-    @IBOutlet var infoLbl: UIImageView!
+    @IBOutlet var hiddenView: UILabel!
+    @IBOutlet var infoLbl: UILabel!
     @IBOutlet var calibrateBtn: UIButton!
     @IBOutlet var imageView: UIImageView!
 
+    
+    private var savedStableState: [String : (x:Int , y:Int, z:Int)] = Dictionary()
+    private var nearableManager: ESTNearableManager! = nil
+    private var nearables : [ESTNearable]  = []
+    private let nh = NearableHelper.self
+    private var isCalibrated = false;
+    
+    
+    let imgBag = UIImage(named: "sticker_bag")
+    let imgDoor = UIImage(named: "sticker_door")
+    let imgFridge = UIImage(named: "sticker_fridge")
+    let imgPTC = UIImage(named: "ptc")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.image = imgPTC
+        nearableManager = ESTNearableManager()
+        nearableManager.delegate = self
+        nearableManager.startRangingForType(ESTNearableType.All)
+        
     }
+    
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -30,8 +52,65 @@ class ViewController: UIViewController, ESTNearableManagerDelegate {
         
         popController.popoverContentSize = CGSize(width: 600, height: 600)
         
-        popController.presentPopoverFromRect(sender.frame , inView: self.view , permittedArrowDirections: UIPopoverArrowDirection.allZeros, animated: true)
+        popController.presentPopoverFromRect(hiddenView.frame , inView: self.view , permittedArrowDirections: UIPopoverArrowDirection.Down, animated: true)
     }
     
+    
+    //nearableManagerDelegates
+    func nearableManager(manager: ESTNearableManager!,
+        didRangeNearables nearables: [AnyObject]!,
+        withType type: ESTNearableType) {
+            
+            self.nearables = nearables as! Array <ESTNearable>
+            var n = nearables as! Array <ESTNearable>
+            var anyMoved = false
+            for (s,e) in savedStableState {
+                if isCalibrated {
+                    if var moved = nh.hasStickerMoved(s, currentNearableArr : self.nearables , savedNearableArr : savedStableState ){  //if moving item is in savedStableState
+                        if moved {
+                            var text = ""
+                            switch s {
+                            case "9b3cd4460c56565e":
+                                imageView.image = imgFridge
+                            case "d59a7be8987f28a4" :
+                                imageView.image = imgDoor
+                            case "8b386da476667197" :
+                                imageView.image = imgBag
+                            case "9b3cd4460c56565e" :
+                                imageView.image = imgBag
+                            case "7830f21de0035fe3" :
+                                imageView.image = imgDoor
+                            default :
+                                print("new item found : \(s)");
+                            }
+                            
+                            infoLbl.text = nh.getNameFromId(s);
+                            anyMoved = true;
+                            break ; // break out of loop to prevent flashing
+                        }
+                    }
+                    else {//if the moving item is not in savedStableState
+                        infoLbl.text = "moving label not in savedStableState"
+                    }
+                }
+            }
+            
+            if !anyMoved {
+                infoLbl.text = "nothing is moving"
+                imageView.image = imgPTC
+                
+            }
+            
+    }
+    @IBAction func onCalibrationClicked(sender: AnyObject) {
+        for n in nearables{
+            savedStableState[n.identifier] = (n.xAcceleration, n.yAcceleration, n.zAcceleration)
+        }
+        infoLbl.text = "calibrated with \(nearables.count) items"
+        
+        if (nearables.count > 0 ) {
+            isCalibrated = true
+        }
+    }
 }
 
---
+++ TriggersExample-Swift/ViewControllerModule.swift
@@ -0,0 +1,74 @@
+//
+//  ViewControllerModule.swift
+//  TriggersExample-Swift
+//
+//  Created by Mike Chen on 5/25/15.
+//  Copyright (c) 2015 Estimote Inc. All rights reserved.
+//
+
+import Foundation
+
+class NearableHelper {
+
+    
+    /**
+    * get the name of the sticker with id
+    **/
+    class func getNameFromId(id:String) -> String {
+        var text = ""
+        switch id {
+        case "9b3cd4460c56565e":
+            text = "fridge"
+        case "d59a7be8987f28a4" :
+            text = "door"
+        case "8b386da476667197" :
+            text = "bag"
+        case "9b3cd4460c56565e" :
+            text = "chair"
+        case "7830f21de0035fe3" :
+            text = "car"
+        default :
+            text = "unknown"
+        }
+        return text
+    }
+    // check if the sticker has moved given the current states of all nearables 
+    // and the previous state of all nearables
+    // return true if tag is not in its original position
+    // false other wise
+    // nil if id of the tag is not recorded when calibrated
+    class func hasStickerMoved(id: String, currentNearableArr : [ESTNearable], savedNearableArr : [String : (x:Int , y:Int, z:Int)] ) -> Bool! {
+        let tolerance = 50 // error range for the accelerations
+        var n = findNearableWithId(id, nearables : currentNearableArr)
+        
+        var (x,y,z) = savedNearableArr[id]!
+        if (savedNearableArr[id] == nil) {return nil}
+        if n == nil { return nil}
+        var name = getNameFromId(id)
+        var x1 = Int(n.xAcceleration)
+        var y1 = Int(n.yAcceleration)
+        var z1 = Int(n.zAcceleration)
+        var saved = sqrt(Float(x*x + y*y + z*z) )
+        var current = sqrt(Float(x1*x1 + y1*y1 + z1*z1))
+        var diff = saved - current
+        
+        if (Int(abs(diff)) <= tolerance) {
+            return false //has not moved
+        }
+        else {
+            return true
+        }
+    }
+    
+    // return the nearable with id in the given nearable array
+    // return nil if not found
+    class func findNearableWithId(id:String, nearables : [ESTNearable]) -> ESTNearable! {
+        for n in nearables {
+            if id == n.identifier {
+                return n
+            }
+        }
+        return nil
+    }
+
+}
